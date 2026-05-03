82fe4c39a1cf7438e84a8d3fc74503f39ab1ec4b
Adn Ubaid <techtiki@hotmail.com>
2026-05-03 14:15:43 +0000

    Added Adsense, theme toggle, text borders

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ lightTheme = true
 
 themeToggle.addEventListener("click", function(){
     if (lightTheme == true){
-        body.style.background = "linear-gradient(to bottom, white 100%, gray 0%) no-repeat fixed"
+        body.style.background = "linear-gradient(to bottom, gray 0%, black 100%) no-repeat fixed"
+        body.style.color = "white"
+        body.style.setProperty("--text-outline-rgb", "0, 0, 0")
         themeToggle.innerText = "Light Mode"
+        lightTheme = false
+    }
+    else{
+        body.style.background = "linear-gradient(to bottom, white 0%, gray 100%) no-repeat fixed"
+        body.style.color = "black"
+        body.style.setProperty("--text-outline-rgb", "255, 255, 255")
+        themeToggle.innerText = "Dark Mode"
+        lightTheme = true
     }
 })
