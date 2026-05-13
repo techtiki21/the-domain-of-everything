06a9c15a79df48565a77591cd76b158069a2e251
Adn Ubaid <techtiki@hotmail.com>
2026-05-13 14:36:36 +0000

    Add template to app

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import os
 import requests
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 WEATHER_KEY = os.getenv('')
+
+@app.route('/')
+def index():
+    return render_template("index.html")
