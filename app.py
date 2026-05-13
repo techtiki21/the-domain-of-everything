a8e1331a1fc77ea3b06879e4b4e66831e75ceced
Adn Ubaid <techtiki@hotmail.com>
2026-05-13 18:18:41 +0000

    Basic weather API call

--- app.py
+++ app.py
@@ -5,9 +5,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+#load env vars
 app = Flask(__name__, template_folder="templates")
-WEATHER_KEY = os.getenv('')
+WEATHER_KEY = os.getenv('WEATHER_KEY')
+
+# weather data
+city = "London"
+weather_response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_KEY}&units=metric")
+weather_data = weather_response.json()
+temperature = weather_data['main']['temp']
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", city=city, temp=temperature)
+
+if __name__ == '__main__':
+    app.run()
