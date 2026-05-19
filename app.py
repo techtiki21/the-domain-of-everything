55b2291e5cf8b69d1dadfafe5d1f8ceca621ad9a
Adn Ubaid <techtiki@hotmail.com>
2026-05-19 14:27:35 +0000

    Completed weather cell

--- app.py
+++ app.py
@@ -13,12 +13,15 @@ def fetchWeatherAPI(city):
     weather_response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_KEY}&units=metric")
     weather_data = weather_response.json()
     temperature = weather_data['main']['temp']
-    return [city, temperature]
+    low_high = f"{weather_data['main']['temp_min']}/{weather_data['main']['temp_max']}"
+    feels = weather_data['main']['feels_like']
+    sky = weather_data['weather'][0]['main']
+    return [city, temperature, feels, sky, low_high]
 
 @app.route('/')
 def index():
     weather = fetchWeatherAPI("London")
-    return render_template("index.html", city=weather[0], temp=weather[1])
+    return render_template("index.html", city=weather[0], temp=weather[1], feelsLike=weather[2], weather=weather[3], lowHigh=weather[4])
 
 if __name__ == '__main__':
     app.run()
